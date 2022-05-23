6bf3a91d0c32f01930c455c99fbc0677892f68a2
GoGei <rich.290401@gmail.com>
2022-05-23 22:57:52 +0300

    Add description

--- Api/documentation/schemas.py
+++ Api/documentation/schemas.py
@@ -18,7 +18,7 @@ schema_view_v1 = get_schema_view(
     openapi.Info(
         title="Superuser API",
         default_version='v1',
-        description='Description',
+        description='API for superusers with full access to every model',
     ),
     url=api_url,
     patterns=[url(r'^v1/', include((router_v1.urls, 'Api'), namespace='api-v1'))],
@@ -30,7 +30,7 @@ schema_view_v2 = get_schema_view(
     openapi.Info(
         title="User API",
         default_version='v1',
-        description='Description',
+        description='API for users to read data and manage with their profiles',
     ),
     url=api_url,
     patterns=[url(r'^v2/', include((router_v2.urls, 'Api'), namespace='api-v2'))],
@@ -42,7 +42,7 @@ schema_view_v3 = get_schema_view(
     openapi.Info(
         title="Staff API",
         default_version='v1',
-        description='Description',
+        description='API for staff with access to provided model and manage them (with GUI)',
     ),
     url=api_url,
     patterns=[url(r'^v3/', include((router_v3.urls, 'Api'), namespace='api-v3'))],
